2939febc6d4f1ed523c7fcb4fd9c63c08d9c9a5b
Nelo <kevinpplle@gmail.com>
2019-12-17 11:03:18 +0100

    added branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import category from './components/category/Category';
 
 function App() {
   return (
     <div className="App">
+        <
         <Navbar />
     </div>
   );
