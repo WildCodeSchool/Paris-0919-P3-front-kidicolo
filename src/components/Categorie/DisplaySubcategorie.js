70f1b3ef66e7d9a622d685dc9a9c448e154bcbce
Cyril Carral <cyril.carral85@gmail.com>
2020-01-06 15:36:15 +0100

    display sub with navbar

--- src/components/Categorie/DisplaySubcategorie.js
+++ src/components/Categorie/DisplaySubcategorie.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import './DisplaySubcategorie.css'
+import React from "react";
+import "./DisplaySubcategorie.css";
 
 const DisplaySubcategorie = subcategorys => {
-                return ( 
-                <div className="containerSubcategory">
-                    <p className="subcategory">{subcategorys.subcategorys.name}</p>
-                </div>
-            )
-        }
+  return (
+    <div className="globalCont">
+      <div className="containerSubcategory">
+        <p className="subcategory">{subcategorys.subcategorys.name}</p>
+      </div>
+    </div>
+  );
+};
 
-export default DisplaySubcategorie 
+export default DisplaySubcategorie;
