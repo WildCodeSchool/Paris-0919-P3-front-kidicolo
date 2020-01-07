6e6ca2a6da9bb1aef34596eebe8aaa0ce38f1b15
Nelo <kevinpplle@gmail.com>
2020-01-07 15:56:39 +0100

    added Homepage file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-
+import Homepage from './components/homepage/Homepage';
 import Categorie from './components/category/Categorie';
 
 const App = () => {
   return (
     <div className="App">
+        <Homepage />
         <Categorie />
         <Navbar />
     </div>
---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Homepage extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Homepage
