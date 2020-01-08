071c91a521028fb1771472514b683b094c503fbd
Nelo <kevinpplle@gmail.com>
2020-01-08 15:45:41 +0100

    added grid on the css

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -6,13 +6,25 @@ export class Homepage extends React.Component {
     render() {
         return (
             <div className="mainContainer">
-                <div className="kidiLogo">
+                {/* <div className="kidiLogo">
                     <img className="logo" src={logo} alt="Logo"/>
+                </div> */}
+                <div className="box1">
+                    <h1>Vêtements</h1>
                 </div>
 
-                <div className="accessCategory">
-
+                <div className="box2">
+                    <h1>Jeux/Jouets</h1>
                 </div>
+
+                <div className="box3">
+                    <h1>Education</h1>
+                </div>
+
+                <div className="box4">
+                    <h1>Puériculture</h1>
+                </div>
+                
             </div>
         )
     }
