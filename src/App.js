bebf0948830683e2652826a5db9590c9e41f19f7
Nelo <kevinpplle@gmail.com>
2020-01-08 15:14:20 +0100

    problem solved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import './App.css';
+
+//import components
 import Navbar from './components/navbar/Navbar';
 import Homepage from './components/homepage/Homepage';
 import Categorie from './components/category/Categorie';
+
+//import csss
+import './App.css';
+
 
 const App = () => {
   return (
