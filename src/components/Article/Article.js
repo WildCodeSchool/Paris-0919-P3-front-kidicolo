afb3b96f5935b03ba7ea54b7fb368e7bc861e21b
Tim <Tim@MacBook-Pro-de-Tim.local>
2020-01-10 10:40:48 +0100

    new css fors articles cards

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -7,12 +7,12 @@ const Article = ({ article }) => {
   return (
       <div className="card my-3 mx-3 text-center">
         <img className="card-img-top" src={article.photourl} alt="Card cap" />
-        <div className="card-body">
-          <p className="card-text">{article.brand}</p>
+        <div className="card-body d-flex flex-column">
+          <p className="card-text ">{article.brand}</p>
           <h5 className="card-title">{article.name}</h5>
           <p className="card-text">{article.description}</p>
           <h5 className="card-title">{article.price}€</h5>
-          <a href="..." className="btn btn-primary">Voir l'article</a>
+          <a href="..." className="align-self-end btn btn-block btn btnArticle">Voir l'article</a>
         </div>
       </div>
     
