10dff3ffabae532cce43f3112c8f2791f8dfa201
Flauz <florianguillot94@gmail.com>
2020-01-11 13:58:15 +0100

    modification SearchBar

--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import axios from 'axios'
+import ReactSearchBox from 'react-search-box'
 
 class Searchbar extends React.Component {
     state = {
-        suggestion: [],
+        article: [],
         text: '',
 
 
@@ -24,33 +25,74 @@ class Searchbar extends React.Component {
     //         )
     // }
 
-    onSearchChange = event => {
-        if (event.target.value.length === 0) {
-            this.setState({ movies: [] })
-        } else {
-            axios
-                .get(`https://hackathon-wild-hackoween.herokuapp.com/movies/search/title/${event.target.value}`)
-                .then((res) => {
-                    this.setState({ movies: res.data.movies, isLoaded: true })
-                    return res
-                })
-                .then((res) => {
-                    this.props.searchMovies(res)
-                    return res
-                })
-        }
+    getArticle = () =>{
+        // Send the request
+        axios.get(`/article/article`)
+            // Extract the DATA from the received response
+            .then(res => this.setState({ article: res.data}))
+            .then(res => console.log( "my state",this.state))
+            // .then(res => console.log(res.data[0].quote))
+        // Use this data to update the state
+
+    }; 
+
+
+
+    handleChange = (event) => {
+        const hChanged = event.target.value
+        this.setState({ text: hChanged})
+    }
+
+    // onSearchChange = event => {
+    //     if (event.target.value.length === 0) {
+    //         this.setState({ category: [] })
+    //     } else {
+    //         axios
+    //             .get(`http://localhost:2121/category/${event.target.value}`)
+    //             .then((res) => {
+    //                 this.setState({ category: res.data.category })
+    //                 return res
+    //             })
+                
+    //     }
+    // }
+
+    componentDidMount(){
+        this.getArticle()
+        console.log(this.state.category)
     }
 
     render() {
 
         return (
-            <div className="container">
-                <label for="searchbar">Search the category:</label>
-                <input type="search" id="searchbar" name="q"
-                    aria-label="Search through site content" />
-
-                <button>Search</button>
+            <div>
+                <ReactSearchBox
+                    placeholder="Recherche"
+                    data={this.category}
+                    onSelect={record => console.log(record)}
+                    onFocus={() => {
+                        console.log('This function is called when is focussed')
+                    }}
+                    onChange={value => console.log(value)}
+                    fuseConfigs={{
+                        threshold: 0.05,
+                    }}
+                    
+                />
             </div>
+
+            // <button onClickconsole.log(category)>button</button>
+
+            // <div>
+            //     <input
+            //         type="text"
+            //         class="searchTerm"
+            //         placeholder="recherche"
+            //         onChange={this.onSearchChange}
+            //         onClick={this.handleClick}
+            //     />
+            // </div>
+            
         )
     }
 }
