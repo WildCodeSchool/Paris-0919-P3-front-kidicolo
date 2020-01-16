80ce317145b7190508d8f25a7e7d6d9309f4a672
Nelo <kevinpplle@gmail.com>
2020-01-16 21:55:10 +0100

    fix

---
+++ src/components/category/DropDownMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {DropdownMultiple, Dropdown} from 'reactjs-dropdown-component';
+
+const DropDownMenu = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DropDownMenu
