19b96a25bc8ff605157d9bbab44b73dde9cf49cf
Flauz <florianguillot94@gmail.com>
2020-01-16 10:55:18 +0100

    modif searchBar

--- src/components/searchBar/Searchbar.js
+++ src/components/searchBar/Searchbar.js
@@ -22,29 +22,8 @@ class Searchbar extends React.Component {
                 
         }
     }
-
-
     render() {
-
         return (
-            // <div>
-            //     <ReactSearchBox
-            //         placeholder="Recherche"
-            //         data={this.category}
-            //         onSelect={record => console.log(record)}
-            //         onFocus={() => {
-            //             console.log('This function is called when is focussed')
-            //         }}
-            //         onChange={value => console.log(value)}
-            //         fuseConfigs={{
-            //             threshold: 0.05,
-            //         }}
-                    
-            //     />
-            // </div>
-
-            // <button onClickconsole.log(category)>button</button>
-
             <div>
                 <input
                     type="text"
