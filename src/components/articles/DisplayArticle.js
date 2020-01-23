6325e06dd8e754ed179e208e86611d52cdefce9e
Tim <Tim@MacBook-Pro-de-Tim.local>
2020-01-23 12:23:07 +0100

    bootstrap card not finished

--- src/components/articles/DisplayArticle.js
+++ src/components/articles/DisplayArticle.js
@@ -15,22 +15,25 @@ const DisplayArticle = ({ article }) => {
 
   console.log("I AM HERE !!!!!!", photo)
   return (
-    <div>
-
-      <div className="card DisplayArticleCard mx-auto my-4">
-        <img className="card-img-top imgCardArticle" src={photo} alt="Card cap"/>
-          <div className="card-body">
-            {/* <p>{article.id_user_vendeur}</p> */}
-            <p >{article.brand}</p>
-            <h5 className="card-title">{article.name}</h5>
-            <p className="card-text">{article.description}</p>
-            <p>{article.id_user_vendeur.firstname}</p>
-            <a href="..." className="btn btn-secondary">Voir l'article</a>
+    <div className='container'>
+      <div className='row justify-content-center'>
+          <div className="card col-4 my-3 mx-3 DisplayArticleCard">
+            <img className="card-img-top imgCardArticle" src={photo} alt="Card cap" />
+            <div className=" card-body">
+              {/* <p>{article.id_user_vendeur}</p> */}
+              <p >{article.brand}</p>
+              <h5 className="card-title">{article.name}</h5>
+              <p className="card-text">{article.description}</p>
+              <p>{article.id_user_vendeur.firstname}</p>
+              <a href="..." className="btn btn-secondary align-self-end btn btn-block btn btnArticle">Voir l'article</a>
+            </div>
           </div>
+        
       </div>
 
-      </div>
-      );
-    };
-    
-    export default DisplayArticle;
+    </div>
+
+  );
+};
+
+export default DisplayArticle;
